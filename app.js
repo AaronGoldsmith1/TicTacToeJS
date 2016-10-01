f9176861b9b0067d2f5f874cac20766074926cff
Aaron Goldsmith <aarongoldsmith@msn.com>
2016-10-01 16:40:45 -0700

    changed game buttons to divs

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
 var counter = 0;
 
+var xImage =
+var oImage =
+
+var boardIndex = document.getElementsByClassName("board");
+
+{
+
+  boardIndex[i] =
+
+}
+
+if user click on square, insert X image
+
+
+var button = document.getElementByID("button1").addEventListener("click", function{
+
+} )
+
+
+var 1 = document.getElementByID("button1")
+var 2 = document.getElementByID("button1")
+var 3
+var 4
+var 5
+var 6
+var 7
+var 8
+var 9
+
+var winningCombinations
+
+var gameSquares = []
