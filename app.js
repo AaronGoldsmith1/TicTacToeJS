866c49d606b6466a3a5a2c51b4ba90cd08cc28c8
Aaron Goldsmith <aarongoldsmith@msn.com>
2016-10-01 18:34:01 -0700

    added player turn functions rough draft

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
+
 var counter = 0;
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var button4 = document.getElementById("button4");
+var button5 = document.getElementById("button5");
+var button6 = document.getElementById("button6");
+var button7 = document.getElementById("button7");
+var button8 = document.getElementById("button8");
+var button9 = document.getElementById("button9");
+var xImage = "images/X.png"
+var oImage = "images/O.png"
+var resetButton = document.getElementByID("newGameButton");
 
-var xImage =
-var oImage =
 
-var boardIndex = document.getElementsByClassName("board");
 
-{
+var playerXMove = function {
+  if (counter == 0) {
 
-  boardIndex[i] =
-
+document.getElementsByClassName("boardButton").addEventListener('click', function {document.getElementsByClassName("boardButton").innerHtml = xImage}
+  }
+  counter++;
+  playerOMove();
 }
 
-if user click on square, insert X image
-
-
-var button = document.getElementByID("button1").addEventListener("click", function{
-
-} )
-
-
-var 1 = document.getElementByID("button1")
-var 2 = document.getElementByID("button1")
-var 3
-var 4
-var 5
-var 6
-var 7
-var 8
-var 9
-
-var winningCombinations
-
-var gameSquares = []
+var playerOMove = function {
+  if (counter == 1) {}
+  document.getElementsByClassName("boardButton").addEventListener('click', function {document.getElementsByClassName("boardButton").innerHtml = oImage}
+    }
+    counter++;
+    playerOMove();
+  }
+counter ++
+playerXMove();
+}
