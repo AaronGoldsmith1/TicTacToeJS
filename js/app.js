5d72a501e028af5ce42f7cd275733bb34f29b6ba
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-01-09 19:35:47 -0800

    firebase addPlayer to game

--- js/app.js
+++ js/app.js
@@ -20,3 +20,44 @@
      $scope.firebase.$child("board").$on('child_changed', function(childSnapshot, prevChildName)  {
         	$timeout($scope.checkBoard,150);
     	});
+
+      $scope.checkPlayers = function(){
+        $("#JoinGameForm").show();
+        $("#info").hide();
+        if ($scope.firebase.players[0].nick == ""){
+            $scope.player = 1;
+            $("#joinGameBtn").attr("disabled", false);
+            return "P1 - Available";
+        }
+        else if ($scope.firebase.players[1].nick == ""){
+            $scope.player = 2;
+            $("#joinGameBtn").attr("disabled", false);
+            return "P2 - Available";
+        }
+        else {
+            $scope.player = 0;
+            $("#joinGameBtn").attr("disabled", true);
+            return "No Slots Available";
+        }
+    }
+
+        $scope.addPlayer = function(){
+            console.log("click");
+            if($scope.nick != ""){
+                if ($scope.player == 1){
+                    $scope.firebase.$child("players").$child("0").$update({"nick":$scope.nick});
+                    $("#JoinGameForm").hide();
+                    $("#info").show();
+                }
+                else if ($scope.player == 2){
+                    var player2 = new Firebase("https://tictactoe123456789.firebaseio.com/players/1");
+                    $scope.firebase.$child("players").$child("1").$update({"nick":$scope.nick});
+                    $("#JoinGameForm").hide();
+                    $("#info").show();
+                    $scope.firebase.turn = 1;
+                    $scope.firebase.$save("turn");
+                }
+            }else{
+                console.log("You need to choose a nickname");
+            }
+        }
